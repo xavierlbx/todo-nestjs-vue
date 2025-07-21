ecd2c17fa96d437b60557d8de8c5db4eb99b821e
Lucas Xavier <lucasbebianolbx@gmail.com>
2025-07-20 21:59:54 -0300

    feat: Adiciona prettier e form de cadastro na tela

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,57 +1,126 @@
 <script setup lang="ts">
-import bgImage from "../assets/backgroundAuth.png";
+import { ref } from 'vue';
+import bgImage from '../assets/backgroundAuth.png';
+
+const isLogin = ref(true);
+
+function toggleForm() {
+  isLogin.value = !isLogin.value;
+}
 </script>
 
 <template>
   <v-app>
     <div
-      class="min-h-screen flex items-center justify-center bg-cover bg-center"
+      class="relative overflow-hidden min-h-screen flex items-center justify-center bg-cover bg-center"
       :style="{ backgroundImage: `url(${bgImage})` }"
     >
-      <div
-        class="bg-black/5 backdrop-blur-lg rounded-xl h-[60vh] p-12 max-w-md w-full text-white flex flex-col justify-between"
-        style="box-shadow: 0 0 30px rgba(0, 0, 0, 0.3)"
-      >
-        <!-- Conteúdo central -->
-        <div class="flex flex-col items-center justify-center flex-grow">
-          <h1 class="text-6xl font-semibold mb-6 font-baloo">Login</h1>
-          <div class="w-full font-nunito">
-            <div class="flex flex-col gap-0.5 mb-4">
-              <label class="text-lg">E-mail</label>
-              <input
-                class="border border-violet-500 rounded-xl bg-black/10 ps-4 py-2 hover:bg-black/20 hover:border-violet-400 focus:bg-purple-500/10"
-                type="email"
-                placeholder="Digite seu email"
-              />
+      <Transition name="slide-horizontal" mode="out-in">
+        <div
+          v-if="isLogin"
+          key="login"
+          class="bg-black/5 backdrop-blur-lg rounded-xl h-[60vh] p-12 max-w-md w-full text-white flex flex-col justify-between"
+          style="box-shadow: 0 0 30px rgba(0, 0, 0, 0.3)"
+        >
+          <!-- Conteúdo LOGIN -->
+          <div class="flex flex-col items-center justify-center flex-grow">
+            <h1 class="text-6xl font-semibold mb-6 font-baloo">Login</h1>
+            <div class="w-full font-nunito">
+              <!-- Campos -->
+              <div class="flex flex-col gap-0.5 mb-4">
+                <label class="text-lg">E-mail</label>
+                <input
+                  class="border border-violet-500 rounded-xl bg-black/10 ps-4 py-2 hover:bg-black/20 hover:border-violet-400 focus:bg-purple-500/10"
+                  type="email"
+                  placeholder="Digite seu email"
+                />
+              </div>
+              <div class="flex flex-col gap-0.5">
+                <label class="text-lg">Senha</label>
+                <input
+                  class="border border-violet-500 rounded-xl bg-black/10 ps-4 py-2 hover:bg-black/20 hover:border-violet-400 focus:bg-purple-500/10"
+                  type="password"
+                  placeholder="Digite sua senha"
+                />
+              </div>
             </div>
-            <div class="flex flex-col gap-0.5">
-              <label class="text-lg">Senha</label>
-              <input 
-                class="border border-violet-500 rounded-xl bg-black/10 ps-4 py-2 hover:bg-black/20 hover:border-violet-400 focus:bg-purple-500/10"
-                type="password"
-                placeholder="Digite sua senha"
-              />
-            </div>
+          </div>
+          <!-- Rodapé LOGIN -->
+          <div class="flex flex-col items-center mt-6">
+            <button
+              class="bg-purple-500 font-baloo font-semibold rounded-full py-1.5 px-8 text-xl hover:bg-purple-600 focus:bg-purple-400"
+            >
+              Entrar
+            </button>
+            <p class="mt-4">
+              Não tem uma conta?
+              <a
+                @click.prevent="toggleForm"
+                class="text-purple-300 underline hover:text-purple-500 focus:text-violet-300 cursor-pointer"
+              >
+                Cadastrar
+              </a>
+            </p>
           </div>
         </div>
 
-        <!-- Rodapé fixo no fim -->
-        <div class="flex flex-col items-center mt-6">
-          <button
-            class="bg-purple-500 font-baloo font-semibold rounded-full py-1.5 px-8 text-xl hover:bg-purple-600 focus:bg-purple-400"
-          >
-            Entrar
-          </button>
-          <p class="mt-4">
-            Não tem uma conta?
-            <a
-              class="text-purple-300 underline hover:text-purple-500 focus:text-violet-300"
-              href=""
-              >Cadastrar</a
+        <!-- Transição para CADASTRO -->
+        <div
+          v-else
+          key="cadastro"
+          class="bg-black/5 backdrop-blur-lg rounded-xl h-[65vh] p-12 max-w-md w-full text-white flex flex-col justify-between"
+          style="box-shadow: 0 0 30px rgba(0, 0, 0, 0.3)"
+        >
+          <!-- Conteúdo CADASTRO -->
+          <div class="flex flex-col items-center justify-center flex-grow">
+            <h1 class="text-6xl font-semibold mb-6 font-baloo">Cadastro</h1>
+            <div class="w-full font-nunito">
+              <!-- Campos -->
+              <div class="flex flex-col gap-0.5 mb-4">
+                <label class="text-lg">E-mail</label>
+                <input
+                  class="border border-violet-500 rounded-xl bg-black/10 ps-4 py-2 hover:bg-black/20 hover:border-violet-400 focus:bg-purple-500/10"
+                  type="email"
+                  placeholder="Digite seu email"
+                />
+              </div>
+              <div class="flex flex-col gap-0.5 mb-4">
+                <label class="text-lg">Senha</label>
+                <input
+                  class="border border-violet-500 rounded-xl bg-black/10 ps-4 py-2 hover:bg-black/20 hover:border-violet-400 focus:bg-purple-500/10"
+                  type="password"
+                  placeholder="Digite sua senha"
+                />
+              </div>
+              <div class="flex flex-col gap-0.5">
+                <label class="text-lg">Confirmar senha</label>
+                <input
+                  class="border border-violet-500 rounded-xl bg-black/10 ps-4 py-2 hover:bg-black/20 hover:border-violet-400 focus:bg-purple-500/10"
+                  type="password"
+                  placeholder="Confirme sua senha"
+                />
+              </div>
+            </div>
+          </div>
+          <!-- Rodapé CADASTRO -->
+          <div class="flex flex-col items-center mt-6">
+            <button
+              class="bg-purple-500 font-baloo font-semibold rounded-full py-1.5 px-8 text-xl hover:bg-purple-600 focus:bg-purple-400"
             >
-          </p>
+              Cadastrar
+            </button>
+            <p class="mt-4">
+              Já possui uma conta?
+              <a
+                @click.prevent="toggleForm"
+                class="text-purple-300 underline hover:text-purple-500 focus:text-violet-300 cursor-pointer"
+              >
+                Login
+              </a>
+            </p>
+          </div>
         </div>
-      </div>
+      </Transition>
     </div>
   </v-app>
 </template>
@@ -73,7 +142,7 @@ import bgImage from "../assets/backgroundAuth.png";
 }
 
 .login-container::before {
-  content: "";
+  content: '';
   position: absolute;
   inset: 0;
   backdrop-filter: blur(10px);
@@ -84,5 +153,21 @@ import bgImage from "../assets/backgroundAuth.png";
 .login-container > * {
   position: relative;
   z-index: 1;
+}
+
+.slide-horizontal-enter-active,
+.slide-horizontal-leave-active {
+  transition: all 0.4s ease;
+  position: absolute;
+  width: 100%;
+}
+
+.slide-horizontal-enter-from {
+  transform: translateX(100%);
+  opacity: 0;
+}
+.slide-horizontal-leave-to {
+  transform: translateX(-100%);
+  opacity: 0;
 }
 </style>
