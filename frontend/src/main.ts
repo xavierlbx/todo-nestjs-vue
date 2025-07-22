9d2b374f085762c0f81bfbe47fd4373eeb954124
Lucas Xavier <lucasbebianolbx@gmail.com>
2025-07-21 21:26:56 -0300

    feat: Adiciona Vue Toast

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,17 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import './style.css'
-import App from './App.vue'
-import router from "../router"
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import './style.css';
+import App from './App.vue';
+import router from '../router';
+import Toast, { POSITION } from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 
-const app = createApp(App)
-const pinia = createPinia()
+const app = createApp(App);
+const pinia = createPinia();
 
-app.use(pinia)
-app.use(router)
-app.mount('#app')
+app.use(Toast, {
+    position: POSITION.TOP_CENTER
+});
+app.use(pinia);
+app.use(router);
+app.mount('#app');
