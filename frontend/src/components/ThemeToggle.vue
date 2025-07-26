f309edc5651b54798a29b469f5a1b40877ebe41f
Lucas Xavier <lucasbebianolbx@gmail.com>
2025-07-26 12:26:07 -0300

    refactor: Limpa tailwind css adiona icons para toggles

--- frontend/src/components/ThemeToggle.vue
+++ frontend/src/components/ThemeToggle.vue
@@ -1,36 +1,41 @@
 <script setup lang="ts">
-import { useThemeStore } from '@/stores/theme'
+import { useThemeStore } from '@/stores/theme';
+import { MoonIcon, SunIcon } from '@heroicons/vue/24/outline';
 
-const themeStore = useThemeStore()
+const themeStore = useThemeStore();
 
 // Carrega o tema salvo ao montar o componente
-themeStore.loadTheme()
+themeStore.loadTheme();
 </script>
 
 <template>
   <div
-    class="fixed top-4 left-1/2 transform -translate-x-1/2 z-50"
+    class="fixed top-4 left-1/2 z-50 -translate-x-1/2 transform"
     aria-label="Toggle dark/light mode"
   >
-  <div class="flex bg-gray-100 dark:bg-gray-700 rounded-full">
-    <button
-      @click="themeStore.setTheme('light')"
-      :class="[
-        'px-4 py-1 rounded-full',
-        themeStore.theme === 'light' ? 'bg-purple-600 text-white text-lg px-6' : 'bg-white dark:bg-gray-700 text-gray-500'
-      ]"
-    >
-      Light
-    </button>
-    <button
-      @click="themeStore.setTheme('dark')"
-      :class="[
-        'px-4 py-1 rounded-full',
-        themeStore.theme === 'dark' ? 'bg-purple-600 text-white text-lg px-6' : 'bg-white dark:bg-gray-700 text-gray-800'
-      ]"
-    >
-      Dark
-    </button>
-  </div>
+    <div class="flex rounded-full bg-gray-100 dark:bg-gray-700">
+      <button
+        @click="themeStore.setTheme('light')"
+        :class="[
+          'rounded-full px-2 py-1',
+          themeStore.theme === 'light'
+            ? 'bg-purple-600 px-3 p-2 text-lg text-white'
+            : 'bg-gray-100 text-gray-500 dark:bg-gray-700',
+        ]"
+      >
+        <SunIcon class="h-5 w-5" />
+      </button>
+      <button
+        @click="themeStore.setTheme('dark')"
+        :class="[
+          'rounded-full px-2 py-1',
+          themeStore.theme === 'dark'
+            ? 'bg-purple-600 px-3 p-2 text-lg text-white'
+            : 'bg-gray-100 text-gray-800 dark:bg-gray-700',
+        ]"
+      >
+        <MoonIcon class="h-5 w-5" />
+      </button>
+    </div>
   </div>
 </template>
--- frontend/src/pages/TodoList.vue
+++ frontend/src/pages/TodoList.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 import backgroundTodo from '../assets/backgroundTodo.png';
-import { MagnifyingGlassIcon, ChevronDownIcon } from '@heroicons/vue/24/outline';
+import { MagnifyingGlassIcon, ChevronDownIcon, TrashIcon } from '@heroicons/vue/24/outline';
 import { PlusIcon } from '@heroicons/vue/24/solid';
 import { Menu, MenuButton, MenuItems, MenuItem, Dialog } from '@headlessui/vue';
 import api from '../api/axios.js';
 import { useToast } from 'vue-toastification';
+import router from '@/router/index.js';
 
 interface formCreateTodo {
   title: string;
@@ -26,6 +27,13 @@ onMounted(() => {
   searchTasks();
 });
 
+const logOut = () => {
+  localStorage.removeItem('token');
+  router.push('/');
+  console.log('Usuário deslogado');
+  toast.success('Você saiu com sucesso!');
+};
+
 const addTask = () => {
   isOpen.value = true;
   console.log('task adicionada');
@@ -34,7 +42,6 @@ const addTask = () => {
 const searchTasks = async () => {
   try {
     const { data } = await api.get('/todos');
-    toast.success('Tarefas retornadas');
     dados.value = data;
     console.log(data);
   } catch (error) {
@@ -57,6 +64,16 @@ const createTask = async () => {
   }
 };
 
+const handleDelete = async (dado: any) => {
+  try {
+    await api.delete(`/todos/${dado.id}`);
+    toast.success('Tarefa deletada com sucesso!');
+    searchTasks();
+  } catch (error) {
+    toast.error('Erro ao deletar tarefa');
+  }
+};
+
 const handleCheckboxChange = async (event: Event, dado: any) => {
   const isChecked = (event.target as HTMLInputElement).checked;
   console.log('Checkbox clicado:', dado, 'Marcado?', isChecked);
@@ -74,7 +91,7 @@ const handleCheckboxChange = async (event: Event, dado: any) => {
   } catch (error) {
     toast.error('Erro ao atualizar tarefa');
   }
-}
+};
 
 function setIsOpen(value) {
   isOpen.value = value;
@@ -84,33 +101,33 @@ function setIsOpen(value) {
 <template>
   <v-app>
     <div
-      class="relative w-full overflow-hidden min-h-screen flex items-center justify-center bg-cover bg-center"
+      class="relative flex min-h-screen w-full items-center justify-center overflow-hidden bg-cover bg-center"
       :style="{ backgroundImage: `url(${backgroundTodo})` }"
     >
       <div
-        class="bg-black/5 backdrop-blur-lg rounded-xl h-[100vh] p-12 max-w-lg w-full text-white flex flex-col items-center"
+        class="flex h-[100vh] w-full max-w-lg flex-col items-center rounded-xl bg-black/5 p-12 text-white backdrop-blur-lg"
         style="box-shadow: 0 0 30px rgba(0, 0, 0, 0.3)"
       >
         <!-- Botão de Sair -->
-        <div class="w-full flex flex-row justify-end">
+        <div @click="logOut()" class="flex w-full flex-row justify-end">
           <p class="font-semibold underline hover:text-lg hover:text-sky-400">Sair</p>
         </div>
 
         <!-- Título de Tarefas -->
-        <h1 class="text-4xl font-semibold font-baloo mb-5">Tarefas</h1>
+        <h1 class="font-baloo mb-5 text-4xl font-semibold">Tarefas</h1>
 
         <!-- Pesquisa e Menu -->
-        <div class="w-full flex flex-row items-center justify-center gap-3 font-nunito">
+        <div class="font-nunito flex w-full flex-row items-center justify-center gap-3">
           <!-- Menu Dropdown -->
           <Menu as="div" class="relative">
             <MenuButton
-              class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-black/20 rounded-md hover:bg-black/30 focus:outline-none"
+              class="inline-flex items-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none"
             >
               Menu
-              <ChevronDownIcon class="w-5 h-5 ml-2" />
+              <ChevronDownIcon class="ml-2 h-5 w-5" />
             </MenuButton>
             <MenuItems
-              class="absolute right-0 mt-2 w-40 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black/5 focus:outline-none z-50"
+              class="absolute right-0 z-50 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
             >
               <div class="px-1 py-1">
                 <MenuItem v-slot="{ active }">
@@ -138,12 +155,12 @@ function setIsOpen(value) {
           </Menu>
 
           <!-- Pesquisar -->
-          <div class="w-full relative">
+          <div class="relative w-full">
             <MagnifyingGlassIcon
-              class="w-5 h-5 text-white absolute left-3 top-1/2 transform -translate-y-1/2 pointer-events-none"
+              class="pointer-events-none absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2 transform text-white"
             />
             <input
-              class="w-full ps-10 border border-stone-500 rounded-full bg-black/10 py-1 hover:bg-black/20 hover:border-violet-400 focus:bg-purple-500/10 text-white"
+              class="w-full rounded-full border border-stone-500 bg-black/10 py-1 ps-10 text-white hover:border-violet-400 hover:bg-black/20 focus:bg-purple-500/10"
               type="text"
               placeholder="Pesquisar"
             />
@@ -155,51 +172,54 @@ function setIsOpen(value) {
           <div
             v-for="(dado, index) in dados"
             :key="index"
-            class="h-[5vh] w-full flex flex-row items-center justify-start bg-neutral-700 mb-4 rounded-xl gap-3 ps-4"
+            class="mb-4 flex h-[5vh] w-full flex-row items-center justify-between gap-3 rounded-xl bg-neutral-700 ps-4"
           >
-            <input
-              type="checkbox"
-              class="w-5 h-5 accent-purple-500"
-              :value="dado.id"
-              :checked="dado.done"
-              @change="handleCheckboxChange($event, dado)"
-            />
-            <h3
-              class="text-xl font-semibold font-nunito truncate max-w-[80%] overflow-hidden whitespace-nowrap"
-            >
-              {{ dado.title }}
-            </h3>
+            <div class="flex w-full flex-row items-center justify-start gap-3 overflow-hidden">
+              <input
+                type="checkbox"
+                class="h-5 w-5 accent-purple-500"
+                :value="dado.id"
+                :checked="dado.done"
+                @change="handleCheckboxChange($event, dado)"
+              />
+              <h3
+                class="font-nunito max-w-[80%] truncate overflow-hidden text-xl font-semibold whitespace-nowrap"
+              >
+                {{ dado.title }}
+              </h3>
+            </div>
+            <TrashIcon @click="handleDelete(dado)" class="me-3 h-5 w-5" />
           </div>
         </div>
 
         <!-- Icone de Adicionar tarefas -->
         <div
-          class="absolute bottom-10 right-10 flex items-center justify-center w-8 h-8 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 hover:scale-110 rounded-full hover:bg-purple-600 bg-purple-500 active:bg-violet-700"
+          class="absolute right-10 bottom-10 flex h-8 w-8 items-center justify-center rounded-full bg-purple-500 hover:scale-110 hover:bg-purple-600 active:bg-violet-700 sm:h-8 sm:w-8 md:h-10 md:w-10 lg:h-12 lg:w-12"
           @click="addTask()"
         >
-          <PlusIcon class="w-8 h-8 sm:w-6 sm:h-6 md:w-8 md:h-8 lg:w-9 lg:h-9" />
+          <PlusIcon class="h-8 w-8 sm:h-6 sm:w-6 md:h-8 md:w-8 lg:h-9 lg:w-9" />
         </div>
 
         <Dialog :open="isOpen" @close="setIsOpen" class="relative z-50">
           <div class="fixed inset-0 bg-black/30" aria-hidden="true" />
 
-          <div class="w-full fixed inset-0 flex items-center justify-center p-4">
+          <div class="fixed inset-0 flex w-full items-center justify-center p-4">
             <div
-              class="w-full max-w-md rounded bg-white p-6 text-black flex flex-col items-center justify-center gap-4"
+              class="flex w-full max-w-md flex-col items-center justify-center gap-4 rounded bg-white p-6 text-black"
             >
               <input
-                class="w-full border border-stone-500 ps-3 py-2 rounded-lg"
+                class="w-full rounded-lg border border-stone-500 py-2 ps-3"
                 v-model="formCreateTodo.title"
                 type="text"
                 placeholder="Digite o título da tarefa"
               />
               <div class="flex flex-row justify-center gap-4">
-                <button class="text-white font-semibold bg-stone-500 rounded-full px-3 py-1">
+                <button class="rounded-full bg-stone-500 px-3 py-1 font-semibold text-white">
                   Voltar
                 </button>
                 <button
                   @click="createTask()"
-                  class="text-white font-semibold bg-purple-500 rounded-full px-3 py-1"
+                  class="rounded-full bg-purple-500 px-3 py-1 font-semibold text-white"
                 >
                   Adicionar tarefa
                 </button>
